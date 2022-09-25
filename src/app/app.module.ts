3f051201c6aca033c03777981b54e4cd14a91b61
RHernandezTL <94205286+RHernandezTL@users.noreply.github.com>
2022-09-25 01:07:26 -0500

    Commit Inicial - Aplicación vacía

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+// import { NgxBarcode6Module } from 'ngx-barcode6';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // NgxBarcode6Module,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/inicio/inicio.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.component.html',
+  styleUrls: ['./inicio.component.css']
+})
+export class InicioComponent implements OnInit {
+  version = VERSION.major //Version 13
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
